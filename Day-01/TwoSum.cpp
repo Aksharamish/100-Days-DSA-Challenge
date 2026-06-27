31e6ce9e04eec758015fd73b2106bd2987c95c3e
Akshara <AKSHARA.12418@stu.upes.ac.in>
2026-06-27 18:14:38 +0530

    Day 1 - Two Sum

---
+++ Day-01/TwoSum.cpp
@@ -0,0 +1,23 @@
+// Day 1 - Two Sum
+// Topic: Arrays + Hashing
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+
+            if (mp.find(complement) != mp.end()) {
+                return {mp[complement], i};
+            }
+
+            mp[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
